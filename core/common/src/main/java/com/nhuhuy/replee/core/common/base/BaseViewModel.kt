f8a73898b5cf5c4d754eaecbd47f9d4c922999c7
Nguyen Nhu Huy <nguyennhuhuy42@gmail.com>
2025-12-27 23:16:44 +0700

    feat(core): Add BaseViewModel for MVI architecture
    Introduces a `BaseViewModel` to establish a base for implementing the Model-View-Intent (MVI) pattern. This addition provides a structured way to handle UI state, actions, and events.
    
    - Defines `UiAction`, `UiState`, and `UiEvent` interfaces to create a clear contract for MVI components.
    - The abstract `BaseViewModel` class manages `UiState` with a `StateFlow` and handles one-time `UiEvent`s using a Kotlin `Channel`.
    - Includes an inline extension function `reduce` for `MutableStateFlow` to simplify state updates.

---
+++ core/common/src/main/java/com/nhuhuy/replee/core/common/base/BaseViewModel.kt
@@ -0,0 +1,34 @@
+package com.nhuhuy.replee.core.common.base
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+
+interface UiAction
+
+interface UiState
+
+interface UiEvent
+
+abstract class BaseViewModel<A: UiAction, E: UiEvent, S: UiState>() : ViewModel(){
+    abstract val state: StateFlow<S>
+    private val _event = Channel<E>()
+    val event = _event.receiveAsFlow()
+
+    fun onEvent(event: E){
+        _event.trySend(event)
+    }
+
+    abstract fun onAction(action: A)
+}
+
+inline fun <T>MutableStateFlow<T>.reduce(
+    block: T.() -> T
+) {
+    return this.update { state ->
+        block(state)
+    }
+}
