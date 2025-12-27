84fc5733334add293548accfe85b73eb714cf746
Nguyen Nhu Huy <nguyennhuhuy42@gmail.com>
2025-12-27 16:10:48 +0700

    chore: initial setup project and gitignore.

---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Replee"
+include(":app")
+
+include(":core:common")
+include(":core:design_system")
+include(":feature_chat")
+include(":feature_auth")
+include(":core:database")
+include(":core:firebase")
