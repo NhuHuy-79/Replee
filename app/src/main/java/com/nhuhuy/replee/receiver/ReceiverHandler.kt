599fc1dca7a82600a0ef57055f1fa54425cceb3b
Nguyen Nhu Huy <nguyennhuhuy42@gmail.com>
2026-01-16 21:59:00 +0700

    refactor(app): - Add field 'status' in new message when it is created. - Add fun 'updateMessageStatus' when fun 'sendMessage' is called.
    - update version of kotlinSerialization and kotlinSerializationCore (and Json)

--- app/src/main/java/com/nhuhuy/replee/receiver/ReceiverHandler.kt
+++ app/src/main/java/com/nhuhuy/replee/receiver/ReceiverHandler.kt
@@ -1,15 +1,18 @@
 package com.nhuhuy.replee.receiver
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationManagerCompat
-import com.nhuhuy.replee.core.common.error_handling.onSuccess
+import com.nhuhuy.replee.core.common.error_handling.onFailureSuspend
 import com.nhuhuy.replee.core.common.error_handling.onSuccessSuspend
 import com.nhuhuy.replee.feature_chat.data.SendMessageService
+import com.nhuhuy.replee.feature_chat.data.SyncManager
 import com.nhuhuy.replee.feature_chat.domain.model.Message
+import com.nhuhuy.replee.feature_chat.domain.model.MessageStatus
 import com.nhuhuy.replee.feature_chat.domain.repository.MessageRepository
 import com.nhuhuy.replee.notification.ConversationNotificationFactory
-import dagger.hilt.android.internal.Contexts
 import dagger.hilt.android.qualifiers.ApplicationContext
 import timber.log.Timber
 import java.util.UUID
@@ -22,6 +25,7 @@ interface ReceiverHandler {
 }
 
 class ReceiverHandlerImp @Inject constructor(
+    private val syncManager: SyncManager,
     private val messageRepository: MessageRepository,
     private val sendMessageService: SendMessageService,
     private val notificationFactory: ConversationNotificationFactory,
@@ -39,17 +43,28 @@ class ReceiverHandlerImp @Inject constructor(
             senderId = senderId,
             receiverId = receiverId,
             content = message,
-            seen = false
+            seen = false,
+            status = MessageStatus.PENDING,
         )
         messageRepository.addNewMessage(conversationId = conversationId, message = newMessage)
             .onSuccessSuspend { message ->
+                syncManager.updateMessageStatusInLocal(
+                    messageId = message.messageId,
+                    status = MessageStatus.SYNCED
+                )
                 sendMessageService.sendMessage(message)
+            }
+            .onFailureSuspend {
+                syncManager.updateMessageStatusInLocal(
+                    messageId = newMessage.messageId,
+                    status = MessageStatus.FAILED
+                )
             }
     }
 
     override fun showNotification(success: Boolean){
         val notification = notificationFactory.showResult(success)
-        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) == android.content.pm.PackageManager.PERMISSION_GRANTED){
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
             NotificationManagerCompat.from(context).notify(notification.hashCode(), notification)
         } else {
             Timber.e("Permission not granted")
